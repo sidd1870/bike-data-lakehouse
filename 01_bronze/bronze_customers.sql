3e4f380a18c648b404ba5ac09b0a1aec9f530ff0
sidd1870 <143423821+sidd1870@users.noreply.github.com>
2026-01-27 11:27:14 +0530

    Update bronze_customers.sql

--- 01_bronze/bronze_customers.sql
+++ 01_bronze/bronze_customers.sql
@@ -1,1 +1,7 @@
+df = (
+    spark.read.option("header", "true")
+    .option("inferSchema", "true")
+    .csv("/Volumes/workspace/bronze/raw_sources/source.crm/cust_info.csv")
+)
 
+df.write.option("mergeSchema", "true").mode("overwrite").saveAsTable("workspace.bronze.crm_cust_info")
