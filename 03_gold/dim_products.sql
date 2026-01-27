afaf89b0130d91d7d46fbbe1f1e9d65fdb88d1a5
sidd1870 <143423821+sidd1870@users.noreply.github.com>
2026-01-27 12:17:31 +0530

    Update dim_products.sql

--- 03_gold/dim_products.sql
+++ 03_gold/dim_products.sql
@@ -1,1 +1,32 @@
+-- Databricks notebook source
+CREATE OR REPLACE TABLE gold.dim_products AS
+SELECT DISTINCT
+    product_key,
+    product_name,
+    product_line,
+    product_cost,
+    start_date,
+    end_date
+FROM silver.crm_prd_info;
+
+
+
+-- COMMAND ----------
+
+SELECT COUNT(*) FROM gold.dim_products;
+
+
+-- COMMAND ----------
+
+SELECT DISTINCT product_key
+FROM gold.fact_sales
+LIMIT 20;
+
+
+-- COMMAND ----------
+
+SELECT COUNT(*)
+FROM gold.fact_sales f
+JOIN gold.dim_products_current p
+  ON f.product_key = p.product_key;
 
