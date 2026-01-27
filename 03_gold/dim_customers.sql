e55cd3b2e537f7147db41acd5fcaba8012c3c427
sidd1870 <143423821+sidd1870@users.noreply.github.com>
2026-01-27 12:16:19 +0530

    Update dim_customers.sql

--- 03_gold/dim_customers.sql
+++ 03_gold/dim_customers.sql
@@ -1,1 +1,21 @@
+-- Databricks notebook source
+CREATE OR REPLACE TABLE gold.dim_customers AS
+SELECT DISTINCT
+    customer_id,
+    first_name,
+    last_name,
+    marital_status,
+    gender,
+    created_date
+FROM workspace.silver.crm_cust_info;
+
+
+-- COMMAND ----------
+
+SELECT * FROM gold.dim_customers;
+
+
+-- COMMAND ----------
+
+SELECT COUNT(*) FROM gold.dim_customers;
 
